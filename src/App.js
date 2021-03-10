1863d1f275c4b161500df2643f092858fb8d6d2e
Shreyas Patel <shreyas.135@gmail.com>
2021-03-10 18:07:43 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        React Build on Github Example
+        </p>
+    </div>
+  );
+}
+
+export default App;
