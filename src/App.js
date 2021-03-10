e0fbec7a9125f121a125ab466709d38b5df30443
Shreyas Patel <shreyas.135@gmail.com>
2021-03-10 18:28:31 +0530

    init

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>
-        Deply React Code On GitHub Pages
-        </p>
+      <p> Deply React Code On GitHub Pages</p>
     </div>
   );
 }
